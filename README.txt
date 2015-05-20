41b5b1ca8be36f6edfc29f392c46d2e5ea19c019
Lamberto <lamberto@RyRII>
2015-05-19 19:43:03 -0600

    2015.05.19 En esta primera version solo coloco la funcion main(int argc, char *argv[]) en el archivo main.c Tambien agrego el archivo README.txt con muy poco contenido por ahora.

---
+++ README.txt
@@ -0,0 +1,4 @@
+2015.05.19
+En esta primera version solo coloco la funcion main() en el 
+archivo main.c para hacer el commit inicial y subir los 
+cambios a github.
---
+++ main.c
@@ -0,0 +1,10 @@
+/**
+ * main.c - El programa principal de la aplicacion.
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  printf("Conteos dinamicos y conteos rapidos\n");
+
+  return 0;
+}
